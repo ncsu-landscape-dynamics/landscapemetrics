04a0911c7ed4ffc53f28a1a2951d2205fd801e65
ChrisJones687 <cjones1688@gmail.com>
2019-05-01 09:28:08 -0400

    Added bins for metrics based on Cushman et. al. 2008

--- data_creation.R
+++ data_creation.R
@@ -16,6 +16,41 @@ writeRaster(landscape, "H:/Team Drives/APHIS  Projects/Papers/landscapemetrics/d
 
 
 ## Run metrics on all 1,000 landscapes
+# Edge contrast metrics based on Cushman et al 2008 (not really applicable to a binary classification as the area is infected or uninfected)
+
+# Patch shape complexity metrics based on Cushman et al 2008
+lsm_c_shape_mn()
+lsm_c_frac_mn()
+
+# Agregation metrics based on Cushman et al 2008
+lsm_c_clumpy()
+lsm_c_pladj()
+lsm_c_cohesion()
+lsm_c_ai()
+
+# nearest neighbor distance metrics based on Cushman et al 2008
+lsm_c_enn_mn()
+
+# Patch dispersion metrics based on Cushman et al 2008
+lsm_c_enn_cv()
+
+# Large patch dominance metrics based on Cushman et al 2008
+lsm_c_lpi()
+lsm_c_core_mn()
+lsm_c_dcore_mn()
+lsm_c_area_mn()
+
+# Neighborhood similarity metrics based on Cushman et al 2008 (non in landscape metrics but has same issue as edge contrast for binary infected or uninfected)
+
+# Patch size variability metrics based on Cushman et al 2008
+lsm_c_area_cv()
+lsm_c_core_cv()
+lsm_c_dcore_cv()
+
+# Edge Patch Density metrics based on Cushman et al 2008
+lsm_c_ed()
+lsm_c_lsi()
+lsm_c_pd()
 
 ## Compare correlation of all metrics
 
